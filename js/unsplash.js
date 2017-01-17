b3eed1c6b97145f295b43751f72105b76fbf4899
Logan McAnsh <logan@mcan.sh>
2017-01-16 21:35:13 -0500

    update .eslintrc, make js actually work
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- js/unsplash.js
+++ js/unsplash.js
@@ -1,11 +1,13 @@
 // @codekit-prepend 'tokens.js';
 
-const url = `https://api.unsplash.com/photos?per_page=30&order_by=popular&client_id=${authToken}`;
+const url = `https://api.unsplash.com/photos?per_page=30&order_by=latest&client_id=${authToken}`;
 const random = Math.floor(Math.random() * 30) + 1;
 const main = document.querySelector('main');
 const download = document.querySelector('#download');
 
 fetch(url)
   .then(blob => blob.json())
-  .then(data => main.style.backgroundImage = `url(${data[random].urls.regular})`)
-  .then(data => download.href = data[random].urls.regular);
+  .then(function randomImage(data) {
+    main.style.backgroundImage = `url(${data[random].urls.regular})`;
+    download.href = data[random].urls.raw;
+  });
