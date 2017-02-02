3a2ee635a95a0a297f49d4a9f75929ee1a6d1283
Logan McAnsh <logan@mcan.sh>
2017-02-01 20:44:51 -0500

    almost successful authentification
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- source/js/unsplash.js
+++ source/js/unsplash.js
@@ -9,7 +9,10 @@ const userName = document.querySelector('#name');
 const likePhoto = document.querySelector('#like');
 const photoLocation = document.querySelector('#location');
 const viewOnUnsplash = document.querySelector('#view');
+const login = document.querySelector('#login');
+const redirectURI = 'http://mcansh.local:5757';
 let photoId;
+let code;
 
 fetch(url)
   .then(blob => blob.json())
@@ -50,6 +53,64 @@ main.addEventListener('click', hideMore);
 function likeThePhoto(event) {
   event.preventDefault();
   likePhoto.href = `https://unsplash.com/photos/${photoId}/like`;
+  this.classList.toggle('liked');
 }
 
 likePhoto.addEventListener('click', likeThePhoto);
+
+login.href = `https://unsplash.com/oauth/authorize?client_id=${authToken}&redirect_uri=${redirectURI}&response_type=code&scope=public+read_user+write_likes`;
+
+if (document.location.search.length) {
+  const urlEnc = window.location.search;
+  const urlDec = decodeURIComponent(urlEnc);
+  const codeParam = /^code=/;
+  const query = urlDec.split('?');
+
+  if (query.length) {
+    const params = query[1].split('&');
+    params.forEach((param) => {
+      if (param.match(codeParam)) {
+        code = param.replace(codeParam, '');
+        code = code.replace('/', '');
+      }
+    });
+  }
+
+  if (code) {
+    console.log(`CODE: ${code}`);
+    window.code = code;
+  } else {
+    console.error(`you're not logged in!`);
+  }
+}
+
+function logMeIn() {
+  fetch('https://unsplash.com/oauth/token', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Origin: `${redirectURI}`,
+      Host: 'api.unsplash.com'
+    },
+    body: JSON.stringify({
+      client_id: `${authToken}`,
+      client_secret: `${applicationSecret}`,
+      redirect_uri: `${redirectURI}`,
+      code: `${code}`,
+      grant_type: 'authorization_code'
+    })
+  })
+  .then((res) => {
+    console.log(res.json());
+  })
+  // .then((resJson) => {
+  //   console.log(resJson);
+  // });
+
+  if (Response.ok) {
+    fetch('https://api.unsplash.com/me')
+      .then(blob => blob.json())
+      .then((data) => { console.log(data); });
+  }
+}
